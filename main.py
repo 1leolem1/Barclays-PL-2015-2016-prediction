c69c8a0a97529bb905198d58a81744e404f5afe5
leopauls <58358116+leopauls@users.noreply.github.com>
2023-08-19 19:17:15 +0100

    2

--- main.py
+++ main.py
@@ -1,12 +1,31 @@
 import requests as r
-import matplotlib.pyplot as plt
 import pandas as pd
 
 API_KEY = '9d27c091e43d4110b0d905be9bb3c2b6'
 headers = {'X-Auth-Token': API_KEY}
-url = 'https://api.football-data.org/v2/competitions/'
+l1_url = 'https://api.football-data.org/v2/competitions/2015/matches'
 
-response = r.get(url, headers=headers)
+response = r.get(l1_url, headers=headers)
 data = response.json()
+matches = pd.DataFrame(data["matches"])
 
-print(str(data))
+# Step 1: Find the Latest Season
+season_ids = [match["season"]["id"] for match in data["matches"]]
+latest_season_id = max(season_ids)
+
+# Step 2: Filter the Matches for the Latest Season
+latest_season_matches = matches[matches['season'].apply(
+    lambda x: x['id']) == latest_season_id]
+
+home_list = []
+away_list = []
+
+# Iterate through filtered matches
+for index, match in latest_season_matches.iterrows():
+    finished = match["status"] == "FINISHED"
+    if finished:
+        home = match["score"]["fullTime"]["homeTeam"]
+        away = match["score"]["fullTime"]["awayTeam"]
+        print(f"{home} - {away}")
+        home_list.append(home)
+        away_list.append(away)
